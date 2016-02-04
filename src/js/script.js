a8cede24419ba5847365ab525a9acec275b6257f
Mike Francis <mikeffrancis@gmail.com>
2016-02-04 16:08:08 +0000

    See ya jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,1 @@
-window.$ = window.jQuery = require('jquery');
-
-require('./../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse');
-require('./../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown');
+require('./_collapse');
