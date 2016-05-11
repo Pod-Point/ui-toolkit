50fb7dc7d0047d92b10e161a3b6eab734d6239bd
Mike Francis <mikeffrancis@gmail.com>
2016-05-11 11:59:26 +0100

    Mega update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-const glob = require('glob');
-const path = require('path');
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const webpack = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
@@ -14,27 +14,28 @@ const config = require('./config');
 gulp.task('css', () => {
     return gulp.src(config.src.root + '/' + config.src.scss + '/**/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed',
-            errLogToConsole: true
-        }))
-        .pipe(autoprefixer())
+        .pipe(sass({ errLogToConsole: true }))
+        .pipe(postcss([
+            autoprefixer({ browsers: ['last 2 versions'] }),
+            cssnano()
+        ]))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(config.dist.root + '/' + config.dist.css))
-        .pipe(browserSync.reload({ stream: true }));
+        .pipe(browserSync.stream());
 });
 
 gulp.task('js-modules', () => {
     return gulp.src(config.src.root + '/' + config.src.js + '/modules/**/*.js')
-        .pipe(babel({ presets: ['es2015', 'stage-2'], ignore: /node_modules/ }))
-        .pipe(gulp.dest(config.dist.root + '/' + config.dist.js + '/modules'));
+        .pipe(babel())
+        .pipe(gulp.dest(config.dist.root + '/' + config.dist.js + '/modules'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('js', () => {
     return gulp.src(config.src.root + '/' + config.src.js + '/**/*.js')
         .pipe(webpack(webpackConfig))
         .pipe(gulp.dest(config.dist.root + '/' + config.dist.js))
-        .pipe(browserSync.reload({ stream: true }));
+        .pipe(browserSync.stream());
 });
 
 gulp.task('copy-fonts', () => {
@@ -47,21 +48,17 @@ gulp.task('copy-images', () => {
         .pipe(gulp.dest(config.dist.root + '/' + config.dist.images));
 });
 
-gulp.task('browser-reload', () => {
-    return browserSync.reload({ stream: true });
-});
-
-gulp.task('watch', () => {
-    browserSync({
-        server: {
-            baseDir: config.dist.root
-        },
+gulp.task('serve', ['default'], () => {
+    return browserSync.init({
+        server: { baseDir: config.dist.root },
         open: false
     });
+});
 
+gulp.task('watch', ['serve'], () => {
     gulp.watch(config.src.root + '/' + config.src.scss + '/' + '/**/*.scss', ['css']);
     gulp.watch(config.src.root + '/' + config.src.js + '/' + '/**/*.js', ['js', 'js-modules']);
-    gulp.watch(config.dist.root + '/*.html', ['browser-reload']);
+    gulp.watch(config.dist.root + '/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['css', 'js', 'copy-fonts', 'copy-images']);
