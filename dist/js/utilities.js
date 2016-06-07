354b200c8d8d0484b79094d175be3c43a962526a
Mike Francis <mikeffrancis@gmail.com>
2016-06-07 10:39:36 +0100

    Added files outside modules to dist

---
+++ dist/js/utilities.js
@@ -0,0 +1,46 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.show = show;
+exports.hide = hide;
+exports.isVisible = isVisible;
+exports.isHidden = isHidden;
+/**
+ * Remove hidden class from element, showing it via CSS.
+ *
+ * @param element
+ */
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+/**
+ * Apply hidden class to element, hiding it via CSS.
+ *
+ * @param element
+ */
+function hide(element) {
+  element.classList.add('hidden');
+}
+
+/**
+ * Check if an element is visible (isn't hidden by CSS).
+ *
+ * @param element
+ * @returns {boolean}
+ */
+function isVisible(element) {
+  return !isHidden(element);
+}
+
+/**
+ * Check if an element is hidden (by CSS).
+ * 
+ * @param element
+ * @returns {boolean}
+ */
+function isHidden(element) {
+  return element.classList.contains('hidden');
+}
